101c1310917f1204d1585bc656fa29a26c73274d
Blake <blakewoo0819@gmail.com>
2021-04-28 01:31:42 +0900

    사칙 연산 추가

--- module/module_function.js
+++ module/module_function.js
@@ -24,30 +24,99 @@ Calc_object.prototype.parser = function () {
     let split_data = raw_data.split()
 }
 
-Calc_object.prototype.get_parsing_data = function () {
+Calc_object.prototype.get_parsed_data = function () {
     return this.refine_data;
 }
 
-Calc_object.prototype.plus =function (input_a,input_b) {
-    return Number(input_a+input_b);
+Calc_object.prototype.plus = function (input_a,input_b) {
+    if(isNumber(input_a) && isNumber(input_b)) {
+        input_a = isMinMax(input_a)
+        input_b = isMinMax(input_b)
+        if(isFinite(input_a+input_b)) {
+            return Number(input_a+input_b);
+        }
+        else if(input_a+input_b>0) {
+            return Infinity;
+        }
+        else {
+            return -Infinity;
+        }
+    }
+    else {
+        return NaN;
+    }
 }
 
 Calc_object.prototype.minus=function(input_a,input_b) {
-    return Number(input_a-input_b);
+    if(isNumber(input_a) && isNumber(input_b)) {
+        input_a = isMinMax(input_a)
+        input_b = isMinMax(input_b)
+        if(isFinite(input_a-input_b)) {
+            return Number(input_a-input_b);
+        }
+        else if(input_a-input_b>0) {
+            return Infinity;
+        }
+        else {
+            return -Infinity;
+        }
+
+    }
+    else {
+        return NaN;
+    }
 }
 
 Calc_object.prototype.mul = function(input_a,input_b) {
-    if(input_a*input_b === Infinity) {
-        return Infinity;
+
+    if(isNumber(input_a) && isNumber(input_b)) {
+        input_a = isMinMax(input_a)
+        input_b = isMinMax(input_b)
+
+        if((!isFinite(input_a)&&input_b===0)||(!isFinite(input_b)&&input_a===0)) {
+            return NaN;
+        }
+        else {
+            if(isFinite(input_a*input_b)) {
+                return Number(input_a*input_b);
+            }
+            else if(input_a*input_b>0) {
+
+                return Infinity;
+            }
+            else {
+                return -Infinity;
+            }
+        }
     }
     else {
-        return Number(input_a*input_b);
+        return NaN;
     }
 }
 
 Calc_object.prototype.div = function(input_a,input_b) {
-    if(input_b!== 0) {
-        return Number(input_a+input_b)
+
+    if(isNumber(input_a) && isNumber(input_b)) {
+
+        input_a = isMinMax(input_a)
+        input_b = isMinMax(input_b)
+        if((!isFinite(input_a)&&input_b===0)||(!isFinite(input_b)&&input_a===0)) {
+            return NaN;
+        }
+        else {
+            if(isFinite(input_a/input_b)) {
+                return Number(input_a/input_b);
+            }
+            else if(input_a>0) {
+                return Infinity;
+            }
+            else if(input_a === 0) {
+                return NaN;
+            }
+            else {
+                return -Infinity;
+            }
+        }
     }
     else {
         return NaN;
@@ -76,15 +145,43 @@ Calc_object.prototype.cos = function (input_a) {
 Calc_object.prototype.tan = function (input_a) {
 
 }
-Calc_object.prototype.arc_tan = function (input_a) {
+Calc_object.prototype.atan = function (input_a) {
 
 }
-Calc_object.prototype.arc_sin = function (input_a) {
+Calc_object.prototype.asin = function (input_a) {
 
 }
-Calc_object.prototype.arc_cos = function (input_a) {
+Calc_object.prototype.acos = function (input_a) {
 
 }
 
+Calc_object.prototype.root = function (input_a) {
+
+}
+
+function isMinMax(input_a) {
+    if(input_a<Number('1e-20') && input_a>0) {
+        input_a =0;
+    }
+    else if(input_a>-Number('1e-20') && input_a<0) {
+        input_a =0;
+    }
+    else if(input_a>Number('1e+21')) {
+        input_a = Infinity;
+    }
+    else if(input_a<-Number('1e+21')) {
+        input_a = -Infinity;
+    }
+    return input_a;
+}
+
+function isNumber (input_a) {
+    if(input_a===null) {
+        return NaN;
+    }
+    else {
+        return !isNaN(input_a)
+    }
+}
 
 module.exports.Calc_object = Calc_object;
--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ const assert = require('assert');
 
 
 describe('Paser Test', function () {
+
     let test_case_input = [
         '2','3','4','5','6','7'
     ]
@@ -35,37 +36,54 @@ describe('One op Functions Test', function () {
 });
 
 describe.only('Two op Functions Test', function () {
+
+    let test = new target.Calc_object();
+
     let test_case_input1 = [
-        '1','',null,undefined,'1000000000000000000000000000000000000000000000000000000000000000','0.00000000000000000000000000000000000000000000002'
+        1
+        ,
+        ,null
+        ,undefined
+        ,1000000000000000000000000000000000000000000000000000000000000000
+        ,0.00000000000000000000000000000000000000000000002
+        ,-1000000000000000000000000000000000000000000000000000000000000000
+        ,-0.00000000000000000000000000000000000000000000002
     ]
     let test_case_input2 = [
-        '22','2','4','5','6','7'
+        0.00000000000000000000000000000000000000000000000002
+        ,1000000000000000000000000000000000000000000000000000000000000000
+        ,null
+        ,undefined
+        ,1
+        ,
+        ,-0.00000000000000000000000000000000000000000000002
+        ,-1000000000000000000000000000000000000000000000000000000000000000
     ]
     let test_case_output_plus = [
-        '23',NaN,NaN,NaN,Infinity,""
+        1,NaN,NaN,NaN,Infinity,NaN,-Infinity,-Infinity
     ];
     let test_case_output_minus = [
-        '2','3','4','5','6','7'
+        1,NaN,NaN,NaN,Infinity,NaN,-Infinity,Infinity
     ];
     let test_case_output_mul = [
-        '2','3','4','5','6','7'
+        0,NaN,NaN,NaN,Infinity,NaN,NaN,NaN
     ];
     let test_case_output_div = [
-        '2','3','4','5','6','7'
+        Infinity,NaN,NaN,NaN,Infinity,NaN,NaN,NaN
     ];
 
     for(let i=0;i<test_case_input1.length;i++) {
         it('plus '+i, function () {
-            assert.strictEqual(target.Calc_object.plus(test_case_input1[i],test_case_input2[i]), test_case_output[i]);
+            assert.strictEqual(test.plus(test_case_input1[i],test_case_input2[i]), test_case_output_plus[i]);
         });
         it('minus '+i, function () {
-            assert.strictEqual(target.Calc_object.plus(test_case_input1[i],test_case_input2[i]), test_case_output[i]);
+            assert.strictEqual(test.minus(test_case_input1[i],test_case_input2[i]), test_case_output_minus[i]);
         });
         it('mul '+i, function () {
-            assert.strictEqual(target.Calc_object.plus(test_case_input1[i],test_case_input2[i]), test_case_output[i]);
+            assert.strictEqual(test.mul(test_case_input1[i],test_case_input2[i]), test_case_output_mul[i]);
         });
         it('div '+i, function () {
-            assert.strictEqual(target.Calc_object.plus(test_case_input1[i],test_case_input2[i]), test_case_output[i]);
+            assert.strictEqual(test.div(test_case_input1[i],test_case_input2[i]), test_case_output_div[i]);
         });
     }
 
