6cad1e44e6768e4425368e9a5d758fbd211b9d53
Blake <blakewoo0819@gmail.com>
2021-06-18 23:00:34 +0900

    프론트 엔드 제작중

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,15 +6,66 @@ window.addEventListener('DOMContentLoaded', function()
 
 
 function btnEventBinder() {
-    let buttons =  document.getElementsByClassName("calc_button")
-    for(let i=0;i<buttons.length;i++) {
-        buttons[i].removeEventListener("click",btnEvent)
-        buttons[i].addEventListener("click",btnEvent)
+    let operationButtons =  document.getElementsByClassName("calc_button")
+    let functionButton = document.getElementsByClassName("function_input_button");
+    for(let i=0;i<operationButtons.length;i++) {
+        operationButtons[i].removeEventListener("click",opBtnEvent)
+        operationButtons[i].addEventListener("click",opBtnEvent)
+    }
+
+    for(let i=0;i<functionButton.length;i++) {
+        functionButton[i].removeEventListener("click",funcBtnEvent)
+        functionButton[i].addEventListener("click",funcBtnEvent)
     }
 }
 
-function btnEvent(event) {
-    alert("aaaa")
-    document.getElementById("math_expression").innerText = "aaaaaa";
-    // document.getElementById("math_expression").innerText += event.currentTarget.value;
+function opBtnEvent(event) {
+    document.getElementById("math_expression").value += event.currentTarget.value;
+}
+
+function funcBtnEvent(event) {
+    let currentValue= event.currentTarget.value;
+    let inputData = "";
+    //sin, cos, tan, asin, acos, atan, root, round, if
+    if(currentValue === "if") {
+        inputData = currentValue + "[,,]"
+    }
+    else if(currentValue === "sin") {
+        inputData = currentValue + "[]"
+    }
+    else if(currentValue === "cos") {
+        inputData = currentValue + "[]"
+    }
+    else if(currentValue === "tan") {
+        inputData = currentValue + "[]"
+    }
+    else if(currentValue === "asin") {
+        inputData = currentValue + "[]"
+    }
+    else if(currentValue === "acos") {
+        inputData = currentValue + "[]"
+    }
+    else if(currentValue === "atan") {
+        inputData = currentValue + "[]"
+    }
+    else if(currentValue === "root") {
+        inputData = currentValue + "[]"
+    }
+    else if(currentValue === "round") {
+        inputData = currentValue + "[,]"
+    }
+    else if(currentValue === "sin") {
+        inputData = currentValue + "[]"
+    }
+
+    document.getElementById("math_expression").value += inputData;
+}
+
+/**
+ * 함수 생성시 포커스 함수
+ * @param number
+ */
+function setCaret(number) {
+    var el = document.getElementById("math_expression")
+
 }
