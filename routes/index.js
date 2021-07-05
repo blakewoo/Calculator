6022176c5c9185b577fcfe6f3ac26f382b604799
Blake <blakewoo0819@gmail.com>
2021-07-05 22:53:43 +0900

    백그라운드 이미지 수정 및 DB 연결 함수 추가(아직 DB 설치 안함) 전체적인 CSS 수정 및 이름 기입란, 이력으로 넘어가는 버튼 추가

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const g_module = require('../module/module_function')
+const db_module = require('../module/db_functions')
 
 var router = express.Router();
 
@@ -15,18 +16,22 @@ router.post('/calculation/module',function (req,res,next) {
   res.send(g_module.calculate(req.body.Data));
 });
 
+// 연산 결과들 불러오기
 router.get('/calculation',function (req,res,next) {
 
 });
 
+// 연산 결과 DB에 넣기
 router.post('/calculation',function (req,res,next) {
 
 });
 
+// 연산 결과 변경하기
 router.put('/calculation',function (req,res,next) {
 
 });
 
+// 연산 결과 삭제하기
 router.delete('/calculation',function (req,res,next) {
 
 });
