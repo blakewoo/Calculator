2b8b532bbd7ed91b33f90ae94945fd48ec70ad52
Blake <blakewoo0819@gmail.com>
2021-07-11 13:42:10 +0900

    계산 결과 표기 및 수정 버튼 추가

--- public/javascripts/logView.js
+++ public/javascripts/logView.js
@@ -1,24 +1,47 @@
 window.addEventListener('DOMContentLoaded', function()
 {
     initLogList ()
+
 });
 
 function initLogList () {
-    let mainTable = document.getElementById("log_view_table");
-    let result = requestGetCalculationLog(function (result) {
-
+    let mainTable = document.getElementsByClassName("log_view_table")[0];
+    requestGetCalculationLog(function (result) {
+        let parsed_result = JSON.parse(result);
         let temp = document.createDocumentFragment();
-        for(let i=0;i<result;i++) {
+        for(let i=0;i<parsed_result.length;i++) {
             let tr = document.createElement("tr");
-            tr.innerHTML = "<td class='log_view_td'>" +result[i].name+
+            tr.id = parsed_result[i]._id
+            tr.innerHTML = "<td>" +"<input class='log_text_box' readOnly value='"+parsed_result[i].name +"'>"+
                 "</td>" +
-                "<td class='log_view_td'>" +result[i].expression+
+                "<td>" +"<input class='log_text_box' readOnly value='"+parsed_result[i].expression+"'>"+
                 "</td>" +
-                "<td class='log_view_td'>" +result[i].result+
+                "<td>" +"<input class='log_text_box' readOnly value='"+parsed_result[i].result+"'>"+
+                "</td>" +
+                "<td><input class='calc_button modify_button' type='button' value='수정'>"+
                 "</td>";
             temp.append(tr);
         }
 
         mainTable.querySelector("tbody").append(temp);
+        dynamicButtonEventBinder()
     })
+}
+
+
+function dynamicButtonEventBinder() {
+    let buttonList = document.getElementsByClassName("modify_button");
+    console.log(buttonList.length)
+    for(let i=0;i<buttonList.length;i++) {
+        buttonList[i].removeEventListener("click",buttonEvent);
+        buttonList[i].addEventListener("click",buttonEvent);
+    }
+}
+
+function buttonEvent(event) {
+    let tr = event.currentTarget.parentNode.parentNode;
+    let textboxes = tr.querySelectorAll(".log_text_box")
+    for (let i = 0; i < textboxes.length; i++) {
+        textboxes[i].readOnly = false;
+    }
 }
