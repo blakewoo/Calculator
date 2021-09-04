04fb683fc14d1097ca0bb0a3d4d89ee982593f77
Blake <blakewoo0819@gmail.com>
2021-09-04 16:59:27 +0900

    modify error message to detail

--- public/javascripts/public_function.js
+++ public/javascripts/public_function.js
@@ -50,15 +50,15 @@ function requestCalculation(data,callback) {
         ["N-1", "연산 가능한 범위를 넘어섰습니다."],
         ["N-2", "소수점의 개수가 적절하지 못합니다."],
         ["N-3", "숫자가 아닙니다."],
-        ["O-1", "연속될 수 없는 연산자가 연속되어 나타났습니다."],
-        ["O-2", "연산자의 인자 형태가 적절하지 않습니다."],
-        ["O-3", "연산자의 형태가 적절하지 않습니다."],
-        ["F-1", "인식 할 수 없는 함수입니다."],
-        ["F-2", "함수의 인자 형태가 적절하지 않습니다."],
-        ["F-3", "함수의 인자 개수가 적절하지 않습니다."],
-        ["F-4", "괄호의 개수가 맞지 않습니다."],
-        ["F-5", "괄호의 형태가 맞지 않습니다."],
-        ["F-6", "괄호안에 인자가 없습니다."],
+        ["O-1", "글자부터 연속될 수 없는 연산자가 연속되어 나타났습니다."],
+        ["O-2", "글자에 있는 연산자의 인자 형태가 적절하지 않습니다."],
+        ["O-3", "글자에 있는 연산자의 형태가 적절하지 않습니다."],
+        ["F-1", "글자는 인식 할 수 없는 함수입니다."],
+        ["F-2", "위치에 있는 함수의 인자 형태가 적절하지 않습니다."],
+        ["F-3", "위치에 있는 함수의 인자 개수가 적절하지 않습니다."],
+        ["F-4", "위치에 있는 괄호의 개수가 맞지 않습니다."],
+        ["F-5", "위치에 있는 괄호의 형태가 맞지 않습니다."],
+        ["F-6", "위치에 있는 괄호안에 인자가 없습니다."],
         ["E-1", "네트워크 연결에 문제가 있습니다."],
         ["E-2", "적합하지 못한 요청입니다."]
     ])
@@ -66,7 +66,12 @@ function requestCalculation(data,callback) {
     ajax_function('POST','/calculation/module',data,function (result){
         let calcResult = JSON.parse(result);
         if(calcResult.isError){
-           return callback(Number(calcResult.error_index+1)+"번째 글자 : "+errorCode.get(calcResult.errorCode).toString());
+            if(isNaN(calcResult.error_index)) {
+                return callback(errorCode.get(calcResult.errorCode).toString());
+            }
+            else{
+                return callback(Number(calcResult.error_index+1)+"번째 "+errorCode.get(calcResult.errorCode).toString());
+            }
         }
         else {
             return callback(calcResult.value.toString())
